4555a2aa08afee465773aefb6ed57ed0e000c703
Philip Ruan <buddhaphilip2015@gmail.com>
2023-01-10 13:15:56 -0500

    solved

---
+++ arrayMult_Solutions.js
@@ -0,0 +1,14 @@
+// arrayMult
+// arrayMult takes an array and a number
+// it returns a new array where each element is the number in the original array multiplied by the passed in number.
+// console.log(arrayMult([1, 2, 3], 2));//[2, 4, 6]
+// console.log(arrayMult([2], 9));//[18]
+// console.log(arrayMult([3, 4], 2));//[6, 8]
+
+function arrayMult(array, num) {
+    return array.map((ele) => ele * num);
+}
+
+console.log(arrayMult([1, 2, 3], 2));//[2, 4, 6]
+console.log(arrayMult([2], 9));//[18]
+console.log(arrayMult([3, 4], 2));//[6, 8]
